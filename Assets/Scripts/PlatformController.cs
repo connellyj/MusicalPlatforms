28f4f27b0679ce873a54e007ec87576188d32968
Julia Connelly <juliakconnelly@gmail.com>
2016-09-23 22:59:55 +0200

    added some files, oops

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Written by Julia Connelly, 9/28/2016
+ * 
+ * A script on a trigger to be attached to the child of a platform
+ * Allows the player to jump through the bottom of the platform and land on top
+ */
+
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour {
+
+    // Ignores collisons between platform and player if player hits trigger
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Player") {
+            Physics2D.IgnoreCollision(transform.parent.GetComponent<Collider2D>(), other.GetComponent<Collider2D>());
+        }
+    }
+
+    // Stops ignoring collsions once the player leaves the trigger
+    void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "Player") {
+            Physics2D.IgnoreCollision(transform.parent.GetComponent<Collider2D>(), other.GetComponent<Collider2D>(), false);
+        }
+    }
+}
---
+++ Assets/Scripts/SongManager.cs
@@ -0,0 +1,60 @@
+﻿/**
+ * Written by Julia Connelly, 9/28/2016
+ * 
+ * Holds, creates, and plays the song created during the game
+ */
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SongManager : MonoBehaviour {
+
+    Queue<AudioClip> song;
+    Queue<float> time;
+
+    static SongManager instance;
+
+
+
+    void Awake() {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+
+
+    void Start() {
+        song = new Queue<AudioClip>();
+        time = new Queue<float>();
+    }
+
+
+
+    // Records a note in the song
+    public static void addNote(AudioClip clip, float timePlayed) {
+        if(GameManager.isGamePlaying()) {
+            instance.song.Enqueue(clip);
+            instance.time.Enqueue(timePlayed);
+        }
+    }
+
+
+
+    // Starts playing the song
+    public static void playSong() {
+        instance.StartCoroutine(instance.startSong());
+    }
+
+
+
+    // Plays the song
+    IEnumerator startSong() {
+        time.Enqueue(0f);
+        foreach(AudioClip clip in instance.song) {
+            AudioSource.PlayClipAtPoint(clip, transform.position);
+            yield return new WaitForSeconds(Mathf.Abs(time.Dequeue() - time.Peek()));
+        }
+        yield return null;
+    }
+}
